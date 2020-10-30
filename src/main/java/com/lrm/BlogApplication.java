d355d5a7d0b9df437034ac4d1fa1d9fcb29a3d48
csh1234 <1397617269@qq.com>
2020-10-31 00:26:17 +0800

    Initial commit

---
+++ src/main/java/com/lrm/BlogApplication.java
@@ -0,0 +1,14 @@
+package com.lrm;
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@NacosPropertySource(dataId = "blog",autoRefreshed = true)
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+}
